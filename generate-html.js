cbcc2197a57e84d19b20f52b27a508556a904402
Ben Trask <bentrask@comcast.net>
2016-05-31 16:00:40 -0400

    generate-html script.

---
+++ generate-html.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+var fs = require("fs");
+
+var prism = require("prismjs");
+require("./node_modules/prismjs/components/prism-makefile");
+
+if(process.argv.length <= 2) {
+	console.log("Usage: generate-html.js path");
+	process.exit(1);
+}
+var path = process.argv[2];
+var str = fs.readFileSync(path, "utf8");
+
+var head =
+	'<!doctype html>\n'+
+	'<meta charset="utf-8">\n'+
+	'<title>make decentralize</title>\n'+
+	'<style>\n'+
+	'body { background: #fff; color: #000; }\n'+
+	'pre { font-family: "Monaco", "Melno", monospace; }\n'+
+	'.comment { color: #444; }\n'+
+	'.symbol { font-weight: 600; }\n'+
+//	'.deps { font-style: italic; }\n'+
+	'</style>\n'+
+	'<code><pre>\n';
+var tail =
+	'</pre></code>';
+
+process.stdout.write(head, "utf8");
+
+var output = prism.highlight(str, prism.languages.makefile);
+process.stdout.write(output, "utf8");
+
+process.stdout.write(tail, "utf8");
+
