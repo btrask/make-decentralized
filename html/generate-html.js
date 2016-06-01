3d323aa6a086c656df4df199156923b939951b9d
Ben Trask <bentrask@comcast.net>
2016-06-01 17:51:32 -0400

    Improve HTML builder.
    Don't leave broken files if we fail partway.

--- html/generate-html.js
+++ html/generate-html.js
@@ -5,13 +5,13 @@ var fs = require("fs");
 var prism = require("prismjs");
 require("./node_modules/prismjs/components/prism-makefile");
 
-if(process.argv.length <= 2) {
-	console.error("Usage: generate-html.js path");
+if(process.argv.length <= 3) {
+	console.error("Usage: generate-html.js head body");
 	process.exit(1);
 }
-var path = process.argv[2];
-var str = fs.readFileSync(path, "utf8");
+var head = fs.readFileSync(process.argv[2], "utf8");
+var body = fs.readFileSync(process.argv[3], "utf8");
 
-var output = prism.highlight(str, prism.languages.makefile);
-process.stdout.write("<code><pre>"+output+"</pre></code>", "utf8");
+var output = prism.highlight(body, prism.languages.makefile);
+process.stdout.write(head+"<pre class='line-numbers'><code>"+output+"</code></pre>", "utf8");
 
