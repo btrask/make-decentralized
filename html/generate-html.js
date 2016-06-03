fc6ceee8f3544d2cd622110031bd6fc716a09c4a
Ben Trask <bentrask@comcast.net>
2016-06-03 16:33:53 -0400

    Improving HTML output, add blank pages for printing.

--- html/generate-html.js
+++ html/generate-html.js
@@ -5,13 +5,14 @@ var fs = require("fs");
 var prism = require("prismjs");
 require("./node_modules/prismjs/components/prism-makefile");
 
-if(process.argv.length <= 3) {
-	console.error("Usage: generate-html.js head body");
+if(process.argv.length <= 4) {
+	console.error("Usage: generate-html.js head body tail");
 	process.exit(1);
 }
 var head = fs.readFileSync(process.argv[2], "utf8");
 var body = fs.readFileSync(process.argv[3], "utf8");
+var tail = fs.readFileSync(process.argv[4], "utf8");
 
 var output = prism.highlight(body, prism.languages.makefile);
-process.stdout.write(head+"<pre class='line-numbers'><code>"+output+"</code></pre>", "utf8");
+process.stdout.write(head+"<pre class='line-numbers'><code>"+output+"</code></pre>"+tail, "utf8");
 
